c21d91255f2a806cd432e63c63bd5cb78fd7f916
Tien Li <tienliquang@gmail.com>
2025-04-06 00:20:55 +0700

    feat: implement a exception model to handle exception raised by app

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,4 +1,10 @@
+import typing as t
+from enum import Enum
+
+import pydantic as p
 from fastapi import status
+from fastapi.responses import JSONResponse
+from starlette.background import BackgroundTask
 
 
 class AppException(Exception):
@@ -25,3 +31,37 @@ class BadRequestError(AppException):
 class UnauthorizedError(AppException):
     def __init__(self, error_message: str) -> None:
         super().__init__(status_code=status.HTTP_401_UNAUTHORIZED, error_message=error_message)
+
+
+class ErrorType(str, Enum):
+    EXCEPTION = "exception_error"
+    AUTHENTICATION = "authentication_error"
+    REQUEST_VALIDATION = "request_validation_error"
+
+
+class ErrorContent(p.BaseModel):
+    error_type: ErrorType = p.Field(alias="error_type")
+    error_message: str | None = p.Field(alias="error_message", default=None)
+    error_details: t.Any | None = p.Field(alias="error_details", default=None)
+
+    model_config = p.ConfigDict(use_enum_values=True)
+
+
+class ErrorContentWithStatusCode(ErrorContent):
+    status_code: int = p.Field(alias="status_code")
+
+
+class ErrorJSONResponse(JSONResponse):
+    def __init__(
+        self,
+        error_content: ErrorContent,
+        status_code: int,
+        headers: t.Mapping[str, str] | None = None,
+        media_type: str | None = None,
+        background: BackgroundTask | None = None,
+    ) -> None:
+        error_content_with_status_code = ErrorContentWithStatusCode(
+            **error_content.model_dump(),
+            status_code=status_code,
+        )
+        super().__init__(error_content_with_status_code.model_dump(), status_code, headers, media_type, background)
