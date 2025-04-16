513085157d72eed291bb0b18d7852814a0441330
Chau Nguyen An Khang <ankhang2603uni@gmail.com>
2025-04-17 04:51:32 +0700

    update: no effect on  non_soft_delete_collections

--- src/common/models/refresh_token.py
+++ src/common/models/refresh_token.py
@@ -5,8 +5,8 @@ import pydantic as p
 from .base import BaseModelWithId, BaseModelWithSoftDelete
 
 
-# class RefreshTokenModel(BaseModelWithId):
-class RefreshTokenModel(BaseModelWithId, BaseModelWithSoftDelete):
+# class RefreshTokenModel(BaseModelWithId, BaseModelWithSoftDelete):
+class RefreshTokenModel(BaseModelWithId):
     hashed_access_token: str = p.Field(alias="hashed_access_token")
     expired_at: datetime = p.Field(alias="expired_at")
     revoked_at: datetime | None = p.Field(alias="revoked_at", default=None)
