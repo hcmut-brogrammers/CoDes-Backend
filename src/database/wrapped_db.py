dafce71766373c9b49811327b1d1e58632a90058
Chau Nguyen An Khang <ankhang2603uni@gmail.com>
2025-04-17 04:33:29 +0700

    update config find_one

--- src/database/wrapped_db.py
+++ src/database/wrapped_db.py
@@ -1,41 +1,42 @@
+from typing import Any, override
+
+from pymongo import MongoClient
 from pymongo.collection import Collection
 from pymongo.cursor import Cursor
 from pymongo.database import Database
 
+IS_DELETED = "is_deleted"
 
-class SoftDeleteCollection:
+
+class SoftDeleteCollection(Collection):
     def __init__(self, collection: Collection):
-        self.collection = collection
+        super().__init__(database=collection._database, name=collection._name, codec_options=collection._codec_options)
 
-    # def find_one(self, filter=None, include_deleted=False, *args, **kwargs):
-    def find_one(self, filter=None, *args, **kwargs):
+    def find_one(self, filter: Any | None = None, *args, **kwargs):
         filter = filter or {}
-        # if not include_deleted:
-        if not "is_deleted" in filter:
-            filter["is_deleted"] = False
-        return self.collection.find_one(filter, *args, **kwargs)
+        if not IS_DELETED in filter:
+            filter[IS_DELETED] = False
+        return super().find_one(filter, *args, **kwargs)
 
-    # def find(self, filter=None, include_deleted=False, *args, **kwargs) -> Cursor:
-    def find(self, filter=None, *args, **kwargs) -> Cursor:
+    def find(self, filter: Any | None = None, *args, **kwargs) -> Cursor:
         filter = filter or {}
-        # if not include_deleted:
-        if not "is_deleted" in filter:
-            filter["is_deleted"] = False
-        return self.collection.find(filter, *args, **kwargs)
-
-    # Forward any other attributes/methods to the real collection
-    def __getattr__(self, item):
-        return getattr(self.collection, item)
+        if not IS_DELETED in filter:
+            filter[IS_DELETED] = False
+        return super().find(filter, *args, **kwargs)
 
 
-class WrappedDatabase:
+class WrappedDatabase(Database):
     def __init__(self, db: Database):
-        self._db = db
+        super().__init__(client=db._client, name=db._name, codec_options=db._codec_options)
 
-    def get_collection(self, name, *args, **kwargs) -> SoftDeleteCollection:
-        raw_collection = self._db.get_collection(name, *args, **kwargs)
+    @override
+    def get_collection(
+        self,
+        name: str,
+        codec_options=None,
+        read_preference=None,
+        write_concern=None,
+        read_concern=None,
+    ) -> SoftDeleteCollection:
+        raw_collection = super().get_collection(name, codec_options)
         return SoftDeleteCollection(raw_collection)
-
-    # Forward everything else
-    def __getattr__(self, name):
-        return getattr(self._db, name)
