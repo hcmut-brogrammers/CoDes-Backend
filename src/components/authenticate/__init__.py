ce83309eb3cae81f185049f76c6d5d937d966e9c
Chau Nguyen An Khang <ankhang2603uni@gmail.com>
2025-04-20 21:31:52 +0700

    add BG-205: Implement API to regenerate access_token whenever user switch from their organization

--- src/components/authenticate/__init__.py
+++ src/components/authenticate/__init__.py
@@ -1,6 +1,10 @@
 from .authenticate_user import AuthenticateUser, AuthenticateUserDep
 from .create_refresh_token import CreateRefreshToken, CreateRefreshTokenDep
 from .refresh_access_token import RefreshAccessToken, RefreshAccessTokenDep
+from .regen_access_token_for_switching_organization import (
+    Regen_access_token_for_switching_organization,
+    Regen_access_token_for_switching_organizationDep,
+)
 from .sign_up import SignUp, SignUpDep
 
 __all__ = [
@@ -12,4 +16,6 @@ __all__ = [
     "CreateRefreshToken",
     "RefreshAccessTokenDep",
     "RefreshAccessToken",
+    "Regen_access_token_for_switching_organization",
+    "Regen_access_token_for_switching_organizationDep",
 ]
