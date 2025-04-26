07c76bc9037b5d5448f4c1e680d34806426c8c77
Chau Nguyen An Khang <ankhang2603uni@gmail.com>
2025-04-26 15:26:29 +0700

    this is final adjustment huhu

--- src/components/join_workspace_invitations/__init__.py
+++ src/components/join_workspace_invitations/__init__.py
@@ -1,8 +1,8 @@
-from .create_join_organization_invitation import CreateJoinOrganizationInvitation, CreateJoinOrganizationInvitationDep
-from .create_multi_join_organization_invitations import (
+from .create_batch_join_organization_invitations import (
     CreateBatchJoinOrganizationInvitation,
     CreateBatchJoinOrganizationInvitationDep,
 )
+from .create_join_organization_invitation import CreateJoinOrganizationInvitation, CreateJoinOrganizationInvitationDep
 
 __all__ = [
     "CreateJoinOrganizationInvitation",
